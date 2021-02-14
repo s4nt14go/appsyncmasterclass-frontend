3ed8dff0400d351d025426b28646063d59b3dd90
s4nt14go <santiagopergamo@gmail.com>
2021-02-14 16:22:50 -0300

    feat(ch5): setup: integrate backend

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+
+Amplify.configure({
+  Auth: {
+    region: process.env.VUE_APP_REGION,
+    userPoolId: process.env.VUE_APP_USER_POOL_ID,
+    userPoolWebClientId: process.env.VUE_APP_USER_POOL_WEB_CLIENT_ID,
+    mandatorySignIn: true,  // we want our users to be signed in before they can interact with our app
+  }
+})
+
+const myAppConfig = {
+  aws_appsync_graphqlEndpoint: process.env.VUE_APP_GRAPHQL_ENDPOINT,
+  aws_appsync_region: process.env.VUE_APP_REGION,
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+}
+
+Amplify.configure(myAppConfig);
 
 Vue.config.productionTip = false
 
