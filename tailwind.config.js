481ba05c1874f6db9380a5bc458dcaac62f3f566
s4nt14go <santiagopergamo@gmail.com>
2021-02-16 07:46:43 -0300

    feat(ch5): landing page: setup and styling with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'blue': '#1da1f2',
+        'darkblue': '#2795D9',
+        'lightblue': '#eff9ff',
+        'dark': '#657786',
+        'light': '#AAB8C2',
+        'lighter': '#E1E8ED',
+        'lightest': '#F5F8FA',
+      }
+    },
   },
   variants: {
     extend: {},
