15e0d15fcd3093df2855972baf107adb7b128caf
s4nt14go <santiagopergamo@gmail.com>
2021-07-25 12:54:29 -0300

    feat(ch5): Home page: rendering the timeline

---
+++ src/components/Tweets.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="tweet in tweets" :key="tweet.id">
+      <Tweet :tweet="tweet" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Tweet from './Tweet.vue'
+import { mapGetters } from 'vuex';
+export default {
+  name: 'Tweets',
+  components: {
+    Tweet
+  },
+  computed: {
+    ...mapGetters('twitter', {
+      nextToken: 'nextTokenTweets'
+    }),
+    ...mapGetters('twitter', [
+      'profile',
+      'tweets',
+    ]),
+  }
+}
+</script>
