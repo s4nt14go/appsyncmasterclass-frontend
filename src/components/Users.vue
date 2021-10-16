a29dd3e9f7d5ca499cc40c325d77daeed1e473cf
s4nt14go <santiagopergamo@gmail.com>
2021-10-16 06:47:53 -0300

    feat(ch5): UI: add loading indicator

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <span v-if="users.length === 0">No results</span>
-    <div v-bind:key="user.id" v-for="user in users">
+    <div v-for="user in users" :key="user.id">
       <User :user="user"/>
     </div>
   </div>
@@ -9,6 +8,7 @@
 
 <script>
 import User from './User.vue';
+
 export default {
   name: 'Users',
   props: ['users'],
